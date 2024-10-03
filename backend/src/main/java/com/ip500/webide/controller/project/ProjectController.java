40d338189c77426ea3b7b5688bc02716930c72bd
minwooKang <rkdalsdn94@gmail.com>
2024-10-04 00:53:34 +0900

    refactor(BE): 기본 주석 스타일을 javadoc 스타일로 변경 (#23)

--- backend/src/main/java/com/ip500/webide/controller/project/ProjectController.java
+++ backend/src/main/java/com/ip500/webide/controller/project/ProjectController.java
@@ -21,22 +21,36 @@ public class ProjectController {
     @Qualifier("mock")
     private final IUserService userService;
 
+    /**
+     * 프로젝트 생성 API
+     * @param request 프로젝트 생성 요청 DTO
+     * @return ProjectResponse 생성된 프로젝트 응답 DTO
+     */
     @PostMapping("/create")
     public ApiResponse<ProjectResponse> createProject(@Valid @RequestBody ProjectCreateRequest request) {
         Long userId = userService.getUserId();
         return ApiResponse.ok(projectService.createProject(userId, request.toServiceRequest()));
     }
 
+    /**
+     * 프로젝트 리스트 조회 API
+     * @return List<ProjectResponse> 프로젝트 응답 DTO 리스트
+     */
     @GetMapping("/list")
     public ApiResponse<List<ProjectResponse>> getProjectListByOwnerId() {
         Long userId = userService.getUserId();
         return ApiResponse.ok(projectService.getProjectListByOwnerId(userId));
     }
 
+    /**
+     * 프로젝트 수정 API
+     * @param projectId 프로젝트 ID
+     * @param request 프로젝트 수정 요청 DTO
+     * @return ProjectResponse 수정된 프로젝트 응답 DTO
+     */
     @PutMapping("/{projectId}")
     public ApiResponse<ProjectResponse> updateProject(@PathVariable Long projectId, @Valid @RequestBody ProjectCreateRequest request) {
         Long userId = userService.getUserId();
         return ApiResponse.ok(projectService.updateProject(userId, projectId, request.toServiceRequest()));
     }
-
 }
